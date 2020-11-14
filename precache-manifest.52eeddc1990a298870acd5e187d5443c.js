c80f0ac0e0a2a5d752c69e8d406d5b18701619ed
ahmcho <mail@ahmcho.com>
2020-11-14 14:26:45 +0400

    Updates

---
+++ precache-manifest.52eeddc1990a298870acd5e187d5443c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dc3ffac14570d4333667bc398e9595",
+    "url": "/gh-portfolio/index.html"
+  },
+  {
+    "revision": "720d5e559d48f5988eaf",
+    "url": "/gh-portfolio/static/js/2.bccd312e.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/gh-portfolio/static/js/2.bccd312e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e6b7d6e650a349e04f",
+    "url": "/gh-portfolio/static/js/main.900b54cf.chunk.js"
+  },
+  {
+    "revision": "8dd171699fdda118d581",
+    "url": "/gh-portfolio/static/js/runtime-main.0ab18eef.js"
+  }
+]);

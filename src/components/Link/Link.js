70c3b15af9787fc82cb07ae3500a3539013de97a
ahmcho <mail@ahmcho.com>
2020-11-14 14:34:35 +0400

    fixed link tag color

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const InnerLink = styled.a`
-    color: #61dafb
+    color: #rgb(35 121 145);
+    text-decoration: none;
 `;
 
 const Link = ({ url, title}) => (
